045c8141ec9d6e6a20615a515dbe329f79e9d961
core23 <mail@core23.de>
2019-03-20 22:18:57 +0100

    [BUILD] Added more tests

---
+++ tests/Core23SitemapBundleTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core23\SitemapBundle\Tests;
+
+use Core23\SitemapBundle\Core23SitemapBundle;
+use Core23\SitemapBundle\DependencyInjection\Compiler\SitemapCompilerPass;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class Core23SitemapBundleTest extends TestCase
+{
+    public function testItIsInstantiable(): void
+    {
+        $bundle = new Core23SitemapBundle();
+
+        $this->assertInstanceOf(Core23SitemapBundle::class, $bundle);
+    }
+
+    public function testBuild(): void
+    {
+        /** @var MockObject&ContainerBuilder $containerBuilder */
+        $containerBuilder = $this->createMock(ContainerBuilder::class);
+
+        $containerBuilder->expects($this->once())->method('addCompilerPass')
+            ->with($this->isInstanceOf(SitemapCompilerPass::class))
+        ;
+
+        $bundle = new Core23SitemapBundle();
+        $bundle->build($containerBuilder);
+    }
+}
